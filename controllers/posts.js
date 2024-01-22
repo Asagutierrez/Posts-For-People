2f29937796569b234aec045a488200f3aa4f26d6
itxmiska <gutierrezasa782@gmail.com>
2024-01-22 14:52:37 -0600

    add posts route and posts controller

---
+++ controllers/posts.js
@@ -0,0 +1,20 @@
+import { Post } from '../models/post.js'
+
+function index(req, res) {
+  Post.find({})
+  .then(posts => {
+    res.render('posts/index', {
+      posts,
+      title: posts
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/")
+  })
+}
+
+
+export {
+  index
+}
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,9 @@
 import { Router } from 'express'
-
+import * as postsCtrl from '../controllers/posts.js'
 const router = Router()
 
-
+// GET localhost:3000/posts
+router.get('/', postsCtrl.index)
 
 export {
   router
